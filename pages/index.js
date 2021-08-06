a478740453dc5cd08f164bd223add91252bf1663
Nicholas Griffin <me@nicholasgriffin.co.uk>
2021-08-06 23:12:22 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -123,18 +123,7 @@ function IndexPage({ query, asPath }) {
                 </p>
                 <p>
                   I spend most of my time doing a range of personal web
-                  development projects around the web alongside my work for
-                  Accrosoft, where I work as a developer and deliver a range of
-                  web-based projects for our customers around the globe.
-                </p>
-                <p>
-                  Before I started at Accrosoft, I worked for myself on various
-                  freelance web development projects and on my own technology
-                  news & reviews site that was called TechNutty (still not a fan
-                  of the name). I also spent a few years at the University of
-                  Derby, mostly drinking and playing Xbox, occasionally I
-                  studied Business Management and somehow managed to achieve a
-                  2:1, which is fair enough. 😂
+                  development projects around the web alongside my day job.
                 </p>
                 <p>
                   I aim to use this website to not only showcase myself but also
